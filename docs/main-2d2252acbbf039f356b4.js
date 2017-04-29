71b5a44b20b8c1e6a5bd314b331af8c4c38f1674
ertrzyiks <mateusz.derks@gmail.com>
2017-04-29 21:33:30 +0200

    Rebuild

--- docs/main-2d2252acbbf039f356b4.js
+++ docs/main-2d2252acbbf039f356b4.js
@@ -43859,6 +43859,7 @@ var physicsWorld
 
 var pos = new THREE.Vector3()
 var quat = new THREE.Quaternion()
+var transformAux1 = new Ammo.btTransform()
 
 const gravityConstant = -9.8
 const margin = 0.05;
@@ -43922,16 +43923,22 @@ function createObjects() {
   })
 
   // Ramp
-  pos.set( 3, 1, 0 )
-  quat.setFromAxisAngle( new THREE.Vector3(0, 0, 1), 30 * Math.PI / 180)
-  var obstacle = createParalellepiped(10, 1, 4, 0, pos, quat, new THREE.MeshPhongMaterial({ color: 0x606060 }))
+  pos.set(3, 10, 0)
+  quat.setFromAxisAngle(new THREE.Vector3(0, 0, 1), 30 * Math.PI / 180)
+  var obstacle = createParalellepiped(10, 1, 4, 1, pos, quat, new THREE.MeshPhongMaterial({ color: 0x606060 }))
   obstacle.castShadow = true
   obstacle.receiveShadow = true
+
+  console.log(obstacle.userData.physicsBody.applyCentralForce.toString())
+
+  setInterval(() => {
+    obstacle.userData.physicsBody.applyCentralForce(new Ammo.btVector3(0, 500, 0))
+  }, 3000)
 }
 
 function createParalellepiped(sx, sy, sz, mass, pos, quat, material) {
-  var threeObject = new THREE.Mesh(new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1 ), material)
-  var shape = new Ammo.btBoxShape(new Ammo.btVector3(sx * 0.5, sy * 0.5, sz * 0.5 ))
+  const threeObject = new THREE.Mesh(new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1 ), material)
+  const shape = new Ammo.btBoxShape(new Ammo.btVector3(sx * 0.5, sy * 0.5, sz * 0.5 ))
   shape.setMargin(margin)
 
   createRigidBody(threeObject, shape, mass, pos, quat)
@@ -43949,16 +43956,16 @@ function updatePhysics( deltaTime ) {
   physicsWorld.stepSimulation( deltaTime, 10 )
 
   // Update rigid bodies
-  for ( var i = 0, il = rigidBodies.length; i < il; i++ ) {
-    var objThree = rigidBodies[ i ];
-    var objPhys = objThree.userData.physicsBody;
-    var ms = objPhys.getMotionState();
-    if ( ms ) {
-      ms.getWorldTransform( transformAux1 );
-      var p = transformAux1.getOrigin();
-      var q = transformAux1.getRotation();
-      objThree.position.set( p.x(), p.y(), p.z() );
-      objThree.quaternion.set( q.x(), q.y(), q.z(), q.w() );
+  for (var i = 0, il = rigidBodies.length; i < il; i++) {
+    var objThree = rigidBodies[i]
+    var objPhys = objThree.userData.physicsBody
+    var ms = objPhys.getMotionState()
+    if (ms) {
+      ms.getWorldTransform(transformAux1)
+      var p = transformAux1.getOrigin()
+      var q = transformAux1.getRotation()
+      objThree.position.set(p.x(), p.y(), p.z())
+      objThree.quaternion.set(q.x(), q.y(), q.z(), q.w())
     }
   }
 }
@@ -43966,24 +43973,28 @@ function updatePhysics( deltaTime ) {
 function createRigidBody(threeObject, physicsShape, mass, pos, quat) {
   threeObject.position.copy( pos )
   threeObject.quaternion.copy( quat )
-  var transform = new Ammo.btTransform()
-  transform.setIdentity()
-  transform.setOrigin( new Ammo.btVector3( pos.x, pos.y, pos.z ) )
-  transform.setRotation( new Ammo.btQuaternion( quat.x, quat.y, quat.z, quat.w ) )
-  var motionState = new Ammo.btDefaultMotionState( transform )
-  var localInertia = new Ammo.btVector3( 0, 0, 0 )
-  physicsShape.calculateLocalInertia( mass, localInertia )
-  var rbInfo = new Ammo.btRigidBodyConstructionInfo( mass, motionState, physicsShape, localInertia )
-  var body = new Ammo.btRigidBody( rbInfo )
-  threeObject.userData.physicsBody = body
-  scene.add( threeObject )
 
-  if ( mass > 0 ) {
-    rigidBodies.push( threeObject );
+  const transform = new Ammo.btTransform()
+  transform.setIdentity()
+  transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z))
+  transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w))
+
+  const motionState = new Ammo.btDefaultMotionState(transform)
+  const localInertia = new Ammo.btVector3(0, 0, 0)
+  physicsShape.calculateLocalInertia(mass, localInertia)
+
+  const rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, physicsShape, localInertia)
+  const body = new Ammo.btRigidBody(rbInfo)
+  body.setRestitution(0.4)
+  threeObject.userData.physicsBody = body
+  scene.add(threeObject)
+
+  if (mass > 0) {
+    rigidBodies.push(threeObject)
     // Disable deactivation
-    body.setActivationState( 4 );
+    body.setActivationState(4)
   }
-  physicsWorld.addRigidBody( body );
+  physicsWorld.addRigidBody(body)
   return body;
 }
 
