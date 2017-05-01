8b4c907555e7938f08bdb0443882b1da5e673812
ertrzyiks <mateusz.derks@gmail.com>
2017-05-01 19:37:15 +0200

    Add forward/backward movement

--- src/world.js
+++ src/world.js
@@ -6,8 +6,10 @@ const createWorld = () => {
 
   const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.2, 2000)
   camera.position.x = 0
-  camera.position.y = 10
-  camera.position.z = 10
+  camera.position.y = 15
+  camera.position.z = 15
+  camera.lookAt({x: 0, y: 0, z: 0})
+
 
   const renderer = new THREE.WebGLRenderer()
   renderer.setClearColor(0xbfd1e5)
